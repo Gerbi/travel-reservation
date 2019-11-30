741351e04c2bca5fb878c248d23dc2c18f108289
Gerbi Gomez <gerbi@protonmail.com>
2019-11-29 21:18:49 -0600

    Add Tailwind Config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+        spacing: {
+            '72': '18rem',
+            '80': '20rem',
+            '90': '22rem',
+        },
+    }
+  },
+  variants: {},
+  plugins: []
+}
