d72ebe09aaaee09aedc7475605e05eb8a02f4b6b
Gerbi Gomez <gerbi@protonmail.com>
2019-11-29 21:21:36 -0600

    Creating view blade Navbar

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,13 @@
+<nav class="bg-blue-900 shadow py-6">
+    <div class="container mx-auto px-6 md:px-0">
+        <div class="flex items-center justify-center">
+            <div class="mr-6 flex flex-row items-center">
+                <a class="text-lg font-semibold text-gray-100 no-underline" href="/">Travel Mind</a>
+                <a class="ml-6 text-sm font-medium text-gray-200 no-underline" href="/hotels">Browse Hotels</a>
+            </div>
+            <div class="flex-1 text-right">
+                <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="">Logout</a>
+            </div>
+        </div>
+    </div>
+</nav>
