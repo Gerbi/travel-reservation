f8dd9e19b675a6bd8dc5a2e0581b3efdccb0cfd3
Gerbi Gomez <gerbi@protonmail.com>
2019-12-05 17:39:57 -0600

    Add login btn

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,9 +5,17 @@
                 <a class="text-lg font-semibold text-gray-100 no-underline" href="/">Travel Mind</a>
                 <a class="ml-6 text-sm font-medium text-gray-200 no-underline" href="/hotels">Browse Hotels</a>
             </div>
-            <div class="flex-1 text-right">
-                <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="">Logout</a>
-            </div>
+
+            @if()
+                <div class="flex-1 text-right">
+                    @auth
+                        <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('logout') }}">Logout</a>
+                    @else
+                        <a class="no-underline  hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">Login/Signup</a>
+                    @endauth
+                </div>
+            @endif
         </div>
     </div>
 </nav>
+
